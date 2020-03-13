7692a6c3cf1c2dbe8420550ca67de4558f5025c9
Michele Zucca <michele.zucca81@gmail.com>
2020-03-13 14:33:16 +0200

    added toggle navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -14,55 +14,88 @@ import ProjectList from './components/ProjectList';
 import Footer from './components/Footer';
 
 
-const App = () => {
-    const formattedData = navData.map((data) => (
-    <li className="nav-link"><a href={data.link}><i className={data.icon}></i></a></li>))
-  return (
-    <Router>
-    <div className="App">
-      <nav className="navbar">
-        <ul className="nav-links-left">
-          <li>
-            <Link to= "/" className="nav-link">Home</Link>
-          </li>
-          <li>
-            <Link to= "/about" className="nav-link">About</Link>
-          </li> 
-          <li>
-            <Link to= "/apps" className="nav-link">Apps</Link>
-          </li>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isToggleShowing: true,
+      isExitShowing: false,
+      addClass: true,
+      hideClass: true
 
-        </ul>
+    }
+  }
 
-            <ul className="nav-links-right">
-                {formattedData}
-            </ul>
-        </nav>
-
-      {/* Header */}
-      <Switch>
-     
-      {/* Apps Container */}
-      <Route path="/about">
-        <About />
-        </Route>
-
-       <Route path="/apps"> 
-      <ProjectList />
-      </Route>
-
-      <Route path="/">
-      <Home />
-      </Route>
-      {/* Footer */}
-     
-    </Switch>
-    <Footer />
-    </div>
-
-    </Router>
-
-  );
+  toggle = () => {
+    this.setState({addClass: !this.state.addClass, hideClass: !this.state.hideClass, isToggleShowing: false, isExitShowing: true})
+    
 }
 
+  close = () => {
+    this.setState({addClass: !this.state.addClass, isToggleShowing: true, isExitShowing: false})
+
+}
+   
+    render() {
+      const formattedData = navData.map((data) => (
+        <li className="nav-link"><a href={data.link}><i className={data.icon}></i></a></li>))
+      let navLinksClass = ["navbar-links nav-links-left nav-links-right open"];
+    if(this.state.addClass) {
+        navLinksClass.push("open");
+    }
+   
+
+      return (
+        <Router>
+        <div className="App">
+          <nav className="navbar">
+          {this.state.isToggleShowing ? <div className="hamburger-bar hide-desktop show-tablet" id="open-button" onClick={this.toggle}> 
+                  <i className="fas fa-bars fa-2x"></i>
+                  </div> : null }
+                  {this.state.isExitShowing ? <div className="exit" id="exit-button" onClick={this.close}><i className="fas fa-times fa-2x"></i></div> : null }
+                <ul className={navLinksClass.join('')}>
+              <li>
+                <Link to= "/" className="nav-link">Home</Link>
+              </li>
+              <li>
+                <Link to= "/about" className="nav-link">About</Link>
+              </li> 
+              <li>
+                <Link to= "/apps" className="nav-link">Apps</Link>
+              </li>
+              <ul className="social-links">
+                {formattedData}
+              </ul>
+    
+            </ul>
+    
+            </nav>
+    
+          {/* Header */}
+          <Switch>
+         
+          {/* Apps Container */}
+          <Route path="/about">
+            <About />
+            </Route>
+    
+           <Route path="/apps"> 
+          <ProjectList />
+          </Route>
+    
+          <Route path="/">
+          <Home />
+          </Route>
+          {/* Footer */}
+         
+        </Switch>
+        <Footer />
+        </div>
+    
+        </Router>
+    
+      );
+    }
+    
+    }
 export default App;
